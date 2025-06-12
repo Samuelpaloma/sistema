5c69526cedd5a1a6ec5ab5947bdc3e1a727b7d3a
samuel <samupalo3@gmail.com>
2025-06-12 11:28:24 -0500

    ajust

--- demo/src/main/java/com/example/service/userService.java
+++ demo/src/main/java/com/example/service/userService.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class userService {
+public class userService  {
 
     private final Iuser data;
     private final jwtServices jwtService;
